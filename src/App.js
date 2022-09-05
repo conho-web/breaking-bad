2eced44545c7005047ede536c339c7cf36b951ea
conho-web <conho@internet.ru>
2022-09-05 15:57:57 +0300

    CRA: create app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styles from './App.css';
+import './assets/scss/bundle.scss';
+import Card from "./components/molecules/Card";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/molecules/Card.js
@@ -0,0 +1,11 @@
+import './scss/card.scss';
+
+function Card() {
+  return (
+    <div>
+      123
+    </div>
+  );
+}
+
+export default Card;
